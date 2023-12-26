5052450155dfea7de3a4918213a44599df52d41f
robertyen2023 <robertyen2023@gmail.com>
2023-12-26 23:47:29 +0800

    Start solving problem 1155; export problem1155Solution function and apply it in runProblemSolution in index.ts

---
+++ src/1155/index.ts
@@ -0,0 +1,3 @@
+export default () => {
+    console.log('1155. Number of Dice Rolls With Target Sum');
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 "use strict";
 import problem91Solution from './91/index';
+import problem1155Solution from './1155/index';
 
 function runProblemSolution (solution = () => {}) {
     solution();
 }
 
-runProblemSolution(problem91Solution);
+// runProblemSolution(problem91Solution);
+runProblemSolution(problem1155Solution);
