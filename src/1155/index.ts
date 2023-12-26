d936cc5445467da33f149fb1c5823ebdabf2778d
robertyen2023 <robertyen2023@gmail.com>
2023-12-27 03:06:48 +0800

    Fix the syntax issue of Arrow Function

--- src/1155/index.ts
+++ src/1155/index.ts
@@ -2,7 +2,7 @@ export default () => {
     console.log('1155. Number of Dice Rolls With Target Sum');
 }
 
-const numRollsToTarget = (n: number, k: number, target: number): number {
+const numRollsToTarget = (n: number, k: number, target: number): number => {
     // check the analysis again at [https://www.youtube.com/watch?v=hfUxjdjVQN4&ab_channel=NeetCodeIO]
     // cache 1. https://tinyurl.com/ysf4rraz
     // cache 2. https://tinyurl.com/yq7puwmj
