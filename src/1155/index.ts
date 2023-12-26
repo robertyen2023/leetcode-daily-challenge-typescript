b787777673102d2ab126a2fb7530073c7542b8e6
robertyen2023 <robertyen2023@gmail.com>
2023-12-27 01:20:27 +0800

    Add the basic structure, including MODULO and the empty recursive count function

--- src/1155/index.ts
+++ src/1155/index.ts
@@ -1,3 +1,17 @@
 export default () => {
     console.log('1155. Number of Dice Rolls With Target Sum');
 }
+
+const numRollsToTarget = (n: number, k: number, target: number): number {
+    // check the analysis again at [https://www.youtube.com/watch?v=hfUxjdjVQN4&ab_channel=NeetCodeIO]
+    // cache 1. https://tinyurl.com/ysf4rraz
+    // cache 2. https://tinyurl.com/yq7puwmj
+    // cache 3. cache (n, target)
+
+    const MODULO = 10 ** 9 + 7;
+
+    const count = (n: number, target: number): number => {
+    }
+
+    return count(n, target);
+};
